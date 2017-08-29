bc0a49ae435740c00c0f1a8722bcbd0f891e5142
Erik Rodriguez <rodriguez.erik.r@gmail.com>
2017-08-29 15:29:59 -0400

    Removed LastFM track query from artist searches

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ sp.trace = False
 lastfm = LastFM()
 
 class Artist:
-    def __init__(self, search):
+    def __init__(self, artist, search_type='artist'):
         self.type = 'artist'
-        self.artist = search
+        self.artist = artist
         self.name = self.artist['name']
         self.popularity = self.artist['popularity']
         self.genres = ','.join(self.artist['genres']) if len(self.artist['genres']) > 0 else ''
@@ -28,18 +28,22 @@ class Artist:
 
     def search_albums(self):
         album_search = []
+        print('searching artist albums...')
         results = sp.artist_albums(self.artist['id'], album_type='album')
         album_search.extend(results['items'])
         while results['next']: #if multiple spotify pages
             results = sp.next(results)
             album_search.extend(results['items'])
+        print('searching album release dates...')
         for album in album_search: #get release date then sort
             result = sp.album(album['id'])
             album['release_date'] = result['release_date'][:4]
         album_search.sort(key=lambda album:int(album['release_date']))
+        print('creating album classes...')
         for album in album_search: #make each album into class
             name = album['name'].replace(':','')
-            self.albums[name] = Album(self.artist['name'], album)
+            self.albums[name] = Album(self.artist['name'], album, 'artist')
+        print('calculating album plays...')
         max_plays = max([album.plays for album in self.albums.values()])
         for album in self.albums.values():
             album.scaled_plays = album.plays / max_plays
@@ -56,11 +60,12 @@ class Artist:
             print()
 
 class Album:
-    def __init__(self, artist, album):
+    def __init__(self, artist, album, search_type):
         self.type = 'album'
+        self.search_type = search_type
         self.artist = artist
         self.album = album
-        self.name = album['name'].translate(str.maketrans("", "", string.punctuation))
+        self.name = album['name'].translate(str.maketrans("", "", string.punctuation)) #bokeh labels can't handle certian chars
         if 'release_date' in self.album.keys():
             self.release_date = album['release_date']
             self.axis_label = self.name + ' (' + self.release_date + ')'
@@ -68,24 +73,30 @@ class Album:
         self.tracks = OrderedDict()
         self.num_tracks = 0
         self.duration_ms = 0
+        print('getting lastfm album data...')
         self.listeners, self.plays = lastfm.get_album_listeners_plays(artist, album['name'])
         # print('{}: listeners: {}, plays:{}.'.format(self.name, self.listeners, self.plays))
         self.scaled_plays = 0
         self.search_tracks()
 
     def search_tracks(self):
+        print('searching tracks...')
         results = sp.album_tracks(self.album['id'])
+        print('creating track classes...')
         for track in results['items']:
-            self.tracks[track['name']] = Track(self.artist, track)
+            self.tracks[track['name']] = Track(self.artist, track, self.search_type)
             self.duration_ms += track['duration_ms']
         self.num_tracks = len(self.tracks)
+        print('calculating track plays...')
         max_plays = max([track.plays for track in self.tracks.values()])
         for track in self.tracks.values():
-            track.scaled_plays = track.plays / max_plays
+            if self.search_type is 'album':
+                track.scaled_plays = track.plays / max_plays
             track.features['plays'] = track.scaled_plays
         self.calc_features()
 
     def calc_features(self):
+        print('calculating weighted album features...')
         self.features = {'danceability':0, 'energy':0, 'loudness':0, 'mode':0, 'speechiness':0, 'acousticness':0, 'instrumentalness':0,
                                'liveness':0, 'valence':0, 'tempo':0, 'duration_ms':0, 'time_signature':0}
         for key in self.features: #average tracks
@@ -101,14 +112,21 @@ class Album:
             print(key, " : ", round(value,2))
 
 class Track:
-    def __init__(self, artist, track):
+    def __init__(self, artist, track, search_type):
         self.track = track
         self.artist = artist
         self.name = track['name']
         self.axis_label = track['name']
         self.duration_ms = track['duration_ms']
-        self.listeners, self.plays = lastfm.get_track_listeners_plays(artist, track['name'])
-        self.scaled_plays = 0
+        if search_type is 'album':
+            print('getting lastfm track data...')
+            self.listeners, self.plays = lastfm.get_track_listeners_plays(artist, track['name'])
+            self.scaled_plays = 0
+        else:
+            self.listeners = 0
+            self.plays = 0
+            self.scaled_plays = 0
+        print('searching track features...')
         self.features = sp.audio_features(track['id'])[0]
 
     def print_all(self):
@@ -157,20 +175,20 @@ class Graph:
 
 
     def search_spotify(self):
-        print('clicked')
+        print('searching artist/album')
         self.ds = []
         if self.search_select.active == 0:
             results = sp.search(q='artist:' + self.text_input.value, type='artist', limit=1)
             items = results['artists']['items']
             print("searched artist")
             if len(items) > 0:
-                self.ds = Artist(items[0])
+                self.ds = Artist(items[0], 'artist')
         else:
             results = sp.search(q='album:' + self.text_input.value, type='album', limit=1)
             print("searched album")
             items = results['albums']['items']
             if len(items) > 0:
-                self.ds = Album(items[0]['artists'][0]['name'], items[0])
+                self.ds = Album(items[0]['artists'][0]['name'], items[0], 'album')
         if self.ds:
             self.update_data()
 
@@ -199,6 +217,7 @@ class Graph:
         self.update_data()
 
     def update_data(self):
+        print('graphing...')
         data = self.ds.albums if self.ds.type is 'artist' else self.ds.tracks
         x = []
         y = []
@@ -235,6 +254,7 @@ class Graph:
 
 graph = Graph()
 # graph.search_spotify2('the earth is not a cold dead place', 'album')
+# graph.search_spotify2('Flying Lotus', 'artist')
 
 
 

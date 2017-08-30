110d15230e63c0e33f6bac66d100abb95caf8842
KevinJBoyer <kjb2gd@virginia.edu>
2017-08-30 00:08:28 -0400

    Flask files initial commit

--- __init__.py
+++ __init__.py
@@ -6,7 +6,8 @@ if not app.debug:
     import logging
     from logging import FileHandler
     from logging import Formatter
-#TODO: fix permissions on this file to be more restrictive..
+
+    #TODO: fix permissions on this file to be more restrictive..
     file_handler = FileHandler('/var/www/apps/SpotifyGraph/logs/SpotifyGraph.log')
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(Formatter('''%(asctime)s %(levelname)s: %(pathname)s:%(lineno)d %(module)s:%(funcName)s %(message)s'''))

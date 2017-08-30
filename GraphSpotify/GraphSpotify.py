b1ec4f1a73d7fd6f2ea5830f8ab4042a374e611f
KevinJBoyer <kjb2gd@virginia.edu>
2017-08-30 12:04:11 -0400

    Alter logging functionality

--- GraphSpotify/GraphSpotify.py
+++ GraphSpotify/GraphSpotify.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import logging
+from logging import FileHandler
+from logging import Formatter
 from .lastfm import LastFM
 from flask import Flask
 
@@ -8,14 +11,15 @@ app = Flask(__name__)
 # TODO: move to separate configuration file
 LOG_FILE = "/var/www/apps/SpotifyGraph/logs/SpotifyGraph.log"
 
+# TODO: change logging levels based on debug status
+#       and alter this code to appear after enable_logging is defined
 if not app.debug:
-    import logging
-    from logging import FileHandler
-    from logging import Formatter
+    enable_logging(LOG_FILE, logging.DEBUG)
 
+def enable_logging(log_file, log_level):
     # TODO: fix permissions on this file to be more restrictive
-    file_handler = FileHandler(LOG_FILE)
-    file_handler.setLevel(logging.DEBUG)
+    file_handler = FileHandler(log_file)
+    file_handler.setLevel(log_level)
     file_handler.setFormatter(Formatter(
         "%(asctime)s %(levelname)s: %(pathname)s:"
         + "%(lineno)d %(module)s:%(funcName)s %(message)s"))
